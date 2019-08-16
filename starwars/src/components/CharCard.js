7b16593dd6f7c24195457a43ab89e5cf014edee1
74ultra <r.barnhill@protonmail.com>
2019-08-16 10:21:40 -0700

    Characters rendering to page. Installed sass and semantic-ui

--- starwars/src/components/CharCard.js
+++ starwars/src/components/CharCard.js
@@ -1,8 +1,17 @@
 import React from 'react';
 
 function CharCard (props){
+    
+    console.log(props);
+
     return (
-        <div>Hello</div>
+        <div className="card">
+            <h2>{props.name}</h2>
+            <p>Mass: {props.mass} kg</p>
+            <p>Height: {props.height} cm</p>
+            <p>Eye color: {props.eyeColor}</p>
+            <p>Gender: {props.gender}</p>
+        </div>
     )
 }
 
